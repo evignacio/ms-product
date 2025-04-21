0a1812fc463e3b1b477aca21856ef4929658985b
evignacio <reignomo27@hotmail.com>
2025-04-21 16:43:52 -0300

    Implementacao ControllerAdvice

---
+++ src/main/java/com/fiap/product/infrastructure/controller/handler/ControllerErrorHandler.java
@@ -0,0 +1,21 @@
+package com.fiap.product.infrastructure.controller.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerErrorHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(exception.getMessage(), -1));
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException exception) {
+        return ResponseEntity.unprocessableEntity()
+                .body(new ErrorResponse(exception.getMessage(), -2));
+    }
+}
---
+++ src/main/java/com/fiap/product/infrastructure/controller/handler/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.fiap.product.infrastructure.controller.handler;
+
+public record ErrorResponse(String message, long code) {
+}
